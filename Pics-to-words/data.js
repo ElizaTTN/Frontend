678ca5156e1be8eac75199df9c75761e3b81461c
ElizaTTN <work.elizavetatutina@gmail.com>
2025-03-26 14:04:55 +0300

    add project
    project "pics to words"

---
+++ Pics-to-words/data.js
@@ -0,0 +1,81 @@
+const words = [
+  {
+    id: '1',
+    token: 'sleepy_cat',
+    word: 'Sleepy cat',
+    type: 'word'
+  },
+  {
+    id: '2',
+    token: 'sleepy_cat',
+    url: 'img/cats-1.jpg',
+    type: 'image'
+  },
+  {
+    id: '3',
+    token: 'sad_cat',
+    word: 'Sad cat',
+    type: 'word'
+  },
+  {
+    id: '4',
+    token: 'sad_cat',
+    url: 'img/cats-2.jpg',
+    type: 'image'
+  },
+  {
+    id: '5',
+    token: 'cat_sunbathing',
+    word: 'Take sunbath',
+    type: 'word'
+  },
+  {
+    id: '6',
+    token: 'cat_sunbathing',
+    url: 'img/cats-3.jpg',
+    type: 'image'
+  },
+  {
+    id: '7',
+    token: 'serious_cat',
+    word: 'Serious cat',
+    type: 'word'
+  },
+  {
+    id: '8',
+    token: 'serious_cat',
+    url: 'img/cats-4.jpg',
+    type: 'image'
+  },
+  {
+    id: '9',
+    token: 'cat_is_washing',
+    word: 'Taking a shower',
+    type: 'word'
+  },
+  {
+    id: '10',
+    token: 'cat_is_washing',
+    url: 'img/cats-5.jpg',
+    type: 'image'
+  },
+  {
+    id: '11',
+    token: 'surprised_cat',
+    word: 'Surprised cat',
+    type: 'word'
+  },
+  {
+    id: '12',
+    token: 'surprised_cat',
+    url: 'img/cats-6.jpg',
+    type: 'image'
+  }
+];
+
+const selectedItems = ['3', '4'];
+const finishedItems = ['11', '12'];
+
+window.words = words;
+window.selectedItems = selectedItems;
+window.finishedItems = finishedItems;
